c3832a61042170d7f61903a63ff0ce0a63821514
James <103593846@student.swin.edu.au>
2024-03-18 11:45:44 +1100

    Added switches to GC, door prefab & stuff

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,14 @@ using UnityEngine.AI;
 public class GameController : MonoBehaviour
 {
 
+    public GameObject SteelDoorReference;
+
+    int doorCount;
+
+    int maxSwitchesRequired = 6;
+
+    public MonsterBehaviour mh;
+
     public void LoseGame()
     {
         // Pause game & show UI
@@ -15,5 +23,18 @@ public class GameController : MonoBehaviour
     {
         // Pause game & show UI
         Debug.Log("You win");
+    }
+
+    public void ActivateSwitch()
+    {
+        doorCount += 1;
+        if (doorCount == 1)
+        {
+            // Turn the monster on 
+        }
+        if (doorCount == maxSwitchesRequired)
+        {
+            SteelDoorReference.GetComponent<NavMeshObstacle>().enabled = false;
+        }
     }
 }
--- Assets/Scripts/Switch/Switch.cs
+++ Assets/Scripts/Switch/Switch.cs
@@ -6,10 +6,18 @@ public class Switch : MonoBehaviour
     private bool _isActivated = false;
     public bool IsActivated { get => _isActivated; }
 
+
+
     public void Activate()
     {
+        if (_isActivated) return;
         _isActivated = true;
         //Log the switch activation
         Debug.Log("Switch Activated");
+
+        GameController _gameController = GameObject.Find("GameController").GetComponent<GameController>();
+        _gameController.ActivateSwitch();
+
+        // Play sound here
     }
 }
