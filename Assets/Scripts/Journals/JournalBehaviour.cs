93c98a054fd0dde074bce2c1ef8151bca1f062da
James Bui <jtbui20@gmail.com>
2024-03-18 16:48:21 +1100

    Added journal behaviour to journal

--- Assets/Scripts/Journals/JournalBehaviour.cs
+++ Assets/Scripts/Journals/JournalBehaviour.cs
@@ -7,15 +7,23 @@ public class JournalBehaviour : MonoBehaviour
 {
     public Image Journal;
 
+    bool isPlaying = false;
+
     GameController gc;
+    AudioSource clip;
     // Start is called before the first frame update
     void Start()
     {
         if (!gc) gc = GameObject.Find("GameController").GetComponent<GameController>();
+        if (!clip) GetComponent<AudioSource>();
     }
 
     public void ReadJournal()
     {
+        if (isPlaying) return;
 
+        isPlaying = true;
+
+        clip.Play();
     }
 }
